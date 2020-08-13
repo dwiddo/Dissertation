4c4ad0a4be3346954f711defee24faa70b27b609
Dan <a008135@gmail.com>
2020-08-13 18:29:07 +0100

    plots

--- snippets.py
+++ snippets.py
@@ -36,33 +36,53 @@ def compare_centers(atoms, eps=np.float32(1e-5)):
 
 
 
-def read_cif_(path, split_names=None):
+def read_csv_(path, split_names=None, omit_names=None):
 	"""
 	very specific function for reading csv in the format found in
 	Data\parameter_data_csv directory.
 	Optionally split can be a list of names (first column) to seperate out
 	and return in data_, so they can be highlighted on a graph.
 	"""
+	if split_names is None:
+		split_names = []
+	if omit_names is None:
+		omit_names = []
 
 	with open(path) as file:
 		reader = csv.reader(file)
 		next(reader)
 		data = []
+		data_ = []
 
-		if split_names is None:
-			for line in reader:
-				data.append([float(x) for x in line[1:4]])
-			return np.array(data), None
-		else:
-			data_ = []
-			for line in reader:
+		for line in reader:
+			if line[0] not in omit_names:
 				if line[0] in split_names:
 					data_.append([float(x) for x in line[1:4]])
 				else:
 					data.append([float(x) for x in line[1:4]])
-			return np.array(data), np.array(data_)
+	
+	if split_names is None:
+		return np.array(data), None
+	else:
+		return np.array(data), np.array(data_)
 
-def flatten_colour_scatter_label(path, ax, names=None):
+
+		# if split_names is None:
+		# 	for i, line in enumerate(reader):
+		# 		if line[0] not in ignore_names:
+		# 			data.append([float(x) for x in line[1:4]])
+		# 	return np.array(data), None
+		# else:
+		# 	data_ = []
+		# 	for line in reader:
+		# 		if line[0] not in ignore_names:
+		# 			if line[0] in split_names:
+		# 				data_.append([float(x) for x in line[1:4]])
+		# 			else:
+		# 				data.append([float(x) for x in line[1:4]])
+		# 	return np.array(data), np.array(data_)
+
+def flatten_colour_scatter_label(path, ax, highlight_names=None, omit_names=None):
 	"""
 	very specific function, as the above is.
 	Takes path to csv in format found in directory Data\parameter_data_csv
@@ -71,23 +91,23 @@ def flatten_colour_scatter_label(path, ax, names=None):
 	applied according to p3.
 	"""
 
-	data, label_data = read_cif_(path, split_names=names)
+	data, label_data = read_csv_(path, split_names=highlight_names, omit_names=omit_names)
 	X = data[:,0]
 	Y = data[:,1]
 	clr = data[:,2]
 
-	im = ax.scatter(X, Y, s=4, c=clr, cmap='bwr', 
+	im = ax.scatter(X, Y, s=3, c=clr, cmap='inferno',
 				norm=colors.PowerNorm(gamma=0.5)
-				# norm = colors.LogNorm(vmin=clr.min(), vmax=clr.max())
+				# norm=colors.LogNorm(vmin=clr.min(), vmax=clr.max())
 				)
 	if label_data is not None:
 		ax.scatter(label_data[:,0], label_data[:,1], s=4, c='k')
 		# arrived at offsets manually
-		offsets = [(-50,-10), (3,0), (-55,0), (3,0), (3,0), (3,0), (3,0)]
+		offsets = [(-50,-10), (3,0), (-55,0), (-20,-12), (3,0), (3,0), (3,0)]
 
 		X_ = label_data[:,0]
 		Y_ = label_data[:,1]
-		for i, name in enumerate(names):
+		for i, name in enumerate(highlight_names):
 			ax.annotate(name, (X_[i], Y_[i]), xytext=offsets[i], textcoords='offset points')
 	
 	return im
@@ -113,17 +133,20 @@ if __name__ == '__main__':
 	# # data = data[abs(metric-np.mean(metric)) < 4 * np.std(metric)]
 
 
-
 	path = 'Data\parameter_data_csv\p1+p2pow(x,p3).csv'
-	names = ['job_00001', 'job_00014', 'job_00015', 'job_00054', 
-				 'job_00120', 'job_00186', 'job_05926']
+
+	highlight_names = ['job_00001', 'job_00014', 'job_00015', 'job_00054', 'job_00120', 'job_00186', 'job_05926']
+	# omit_names = ['job_04117']
+	omit_names = None
 
 	fig, ax = plt.subplots()
 
-	im = flatten_colour_scatter_label(path, ax, names=names)
+	im = flatten_colour_scatter_label(path, ax, highlight_names=highlight_names, omit_names=omit_names)
 
 	plt.xlabel('a')
 	plt.ylabel('b')
-	fig.colorbar(im, ax=ax)
+	plt.title("a+b*x^p")
+	cbar = fig.colorbar(im, ax=ax)
+	cbar.set_label("p")
 
 	plt.show()
